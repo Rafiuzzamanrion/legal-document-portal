16f72b82265918efd40e9641997af97c5d82948a
Rafiuzzamanrion <rafiujjaman5683@gmail.com>
2025-11-05 21:54:16 +0600

    updated backend route

--- backend/src/routes/api.js
+++ backend/src/routes/api.js
@@ -3,10 +3,6 @@ const router = express.Router();
 const documentController = require('../controllers/documentController');
 const { validateSearchQuery } = require('../middlewares/validator');
 
-
 router.post('/generate', validateSearchQuery, documentController.generate);
-router.get('/documents', documentController.getAllDocuments);
-router.get('/stats', documentController.getStats);
-router.get('/health', documentController.health);
 
 module.exports = router;
